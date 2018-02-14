6eb514a9a5b46aad65b5f415025585783d063484
zerd05 <zerdltc@gmail.com>
2018-02-14 22:54:56 +0300

    Примеры
    Массивы и рандом

---
+++ ZERD/C#/Примеры/Массивы и рандом/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[15000];                //Объявление массива
+            Random rnd = new Random();                   //Рандом
+            for(int i = 0; i<1500;i++)                      
+            {
+                array[i] = rnd.Next(1, 1300);            //Метод Next возвращает рандомное значение в указанных диапозонах
+                Console.WriteLine(array[i]+ " "+ i);     //Вывод массива
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
