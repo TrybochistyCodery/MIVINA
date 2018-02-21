d48cd2c593356381237a9f17f167e78d510e9584
zerd05 <zerdltc@gmail.com>
2018-02-22 00:15:34 +0300

    .
    .

--- ZERD/C ++ obj/Laba 4 (Reg)/Unit1.h
+++ ZERD/C ++ obj/Laba 4 (Reg)/Unit1.h
@@ -7,6 +7,7 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include <Vcl.Mask.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
@@ -16,6 +17,16 @@ __published:	// IDE-managed Components
 	TLabel *Label3;
 	TButton *Button1;
 	TEdit *Edit1;
+	TMaskEdit *MaskEdit1;
+	TMaskEdit *MaskEdit2;
+	TLabel *Label4;
+	TLabel *Label5;
+	TLabel *Label6;
+	TMaskEdit *MaskEdit3;
+	TLabel *Label7;
+	TMaskEdit *MaskEdit4;
+	TLabel *Label8;
+	TEdit *Edit2;
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm1(TComponent* Owner);
