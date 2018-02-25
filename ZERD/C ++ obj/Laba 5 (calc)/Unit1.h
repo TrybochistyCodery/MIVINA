089dcb548087fb3293be0a943f8d87b354834028
zerd05 <zerdltc@gmail.com>
2018-02-25 18:29:37 +0300

    п
    п

---
+++ ZERD/C ++ obj/Laba 5 (calc)/Unit1.h
@@ -0,0 +1,38 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.Buttons.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TBitBtn *BitBtn1;
+	TBitBtn *BitBtn2;
+	TBitBtn *BitBtn3;
+	TBitBtn *BitBtn4;
+	TBitBtn *BitBtn5;
+	TBitBtn *BitBtn6;
+	TBitBtn *BitBtn7;
+	TBitBtn *BitBtn8;
+	TBitBtn *BitBtn9;
+	TBitBtn *BitBtn10;
+	TBitBtn *BitBtn11;
+	TBitBtn *BitBtn12;
+	TBitBtn *BitBtn13;
+	TBitBtn *BitBtn14;
+	TBitBtn *BitBtn15;
+	TBitBtn *BitBtn16;
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
