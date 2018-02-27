02278574216c25abe7685e363ddfc4d7453dc487
trubochist322 <32900270+trubochist322@users.noreply.github.com>
2018-02-27 10:07:33 +0300

    2
    2

---
+++ Dolboeb kolya/repos/tretya proga po drago/ConsoleApp5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите количество чисел для сортировки.");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите числа для сортировки:");
+            int[] mas = new int[N];
+            Random rand = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = rand.Next(101);
+            }
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine(mas[i]);
+            }
+            ViborSort(mas);
+            Console.WriteLine("Отсортированный массив:");
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine(mas[i]);
+            }
+            Console.ReadLine();
+        }
+ static int[] ViborSort(int[] mas)
+        {
+
+            for (int i = 0; i < mas.Length - 1; i++)
+            {
+                //поиск минимального числа
+                int min = i;
+                for (int j = i + 1; j < mas.Length; j++)
+                {
+                    if (mas[j] < mas[min])
+                    {
+                        min = j;
+                    }
+                }
+                //обмен элементов
+                int temp = mas[min];
+                mas[min] = mas[i];
+                mas[i] = temp;
+            }
+            return mas;
+        }
+    }
+}
---
+++ Dolboeb kolya/repos/vtoraya proga dlya drago/ConsoleApp6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] a = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = rand.Next(101);
+            }
+            InsertionSort(a);
+            Console.ReadKey();
+        }
+        public static int[] InsertionSort(int[] a)
+        {
+            Console.WriteLine("Исходный массив - ");
+            for (int i = 1; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            for (int i = 1; i < a.Length; i++)
+            {
+                int key = a[i];//берем карту в правую руку
+                int j = i - 1;//карта с которой начинаем сравнение
+                while (j >= 0 && a[j] > key)
+                {
+                    a[j + 1] = a[j];//смещаем карту на след. позицию
+                    j--;// переходим к следующей сравниваемой карте
+                }
+                a[j + 1] = key;//вставляем карту из правой руки
+            }
+            Console.WriteLine("Отсортированный массив - ");
+            for (int i = 1; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            return a;
+        }
+    }
+} 
